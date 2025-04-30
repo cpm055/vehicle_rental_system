ebcb1b247fde9cf9762db3b690b63c56152e1fb4
vihanga piyumal <162725349+Renzer21@users.noreply.github.com>
2025-04-30 20:30:19 +0530

    Update StaffDahboard.jsx

--- frontend/src/pages/Vehicle_Management/StaffDahboard.jsx
+++ frontend/src/pages/Vehicle_Management/StaffDahboard.jsx
@@ -3,6 +3,8 @@ import { Outlet } from "react-router-dom";
 
 import '../../css/Admin-dashboard/admin-dashboard.css';
 import StaffNav from "../../Components/Staff_Dashboard/StaffNav";
+// import StaffDashboard from "../../Components/Staff_Dashboard/StaffDashboard";
+// import StaffDashboard from "../../Components/Staff_Dashboard/StaffDashboard";        
 
 function StaffDashboard() {
     return (
